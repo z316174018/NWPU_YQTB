fe754ce3553f0b42f97700321aa70ad570ccab08
Alick <61338279+z316174018@users.noreply.github.com>
2022-10-10 13:12:45 +0800

    Update YQTB.py
    修改UA，使用app模式绕过二验

--- YQTB.py
+++ YQTB.py
@@ -1,148 +1,163 @@
-import time
-import requests
-import tkinter as tk
-from tkinter import messagebox
-# 本地Chrome浏览器的静默模式设置：
-from selenium import  webdriver #从selenium库中调用webdriver模块
-from selenium.webdriver.chrome.options import Options # 从options模块中调用Options类
-from selenium.webdriver.common.by import By
-from pathlib import Path
-import zipfile
-import winreg
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+import json
 import re
-import os
+import traceback
+from datetime import datetime
+from turtle import ht
+import time
 
-def original_window(): 
-    enter_w = tk.Tk()
-    enter_w.title('填报信息')
-    enter_w.geometry('750x500')
- 
-    lab_1 = tk.Label(enter_w,width=7,text='用户名',compound='center')
-    lab_1.place(x=200,y=200)
-    
-    lab_2 = tk.Label(enter_w,width=7,text='密码',compound='center')
-    lab_2.place(x=200,y=230)
-    global uesr_name,password
-    user_name = tk.StringVar()
-    password= tk.StringVar()
-    ###########获取登陆账号和密码##########
-    entry = tk.Entry(enter_w,textvariable=user_name) #用户名
-    entry.pack()
-    entry.place(x=310,y=200)
-   
-   
-    entry_1 = tk.Entry(enter_w,show="*",textvariable=password) 
-    #show使密码不可见
-    entry_1.pack()
-    entry_1.place(x=310,y=230)
-   
- 
-    def panduan(a):
-            tk.messagebox.showinfo('^_^','密码正确,开始填报')
-            un=entry.get()#new_window(enter_w)
-            pw=entry_1.get()#check_chrome_driver_update()
-            a = [un,pw]
-            try:
-                yqtb_nwpu(a)
-            except:
-                get_latest_chrome_driver(get_chrome_version())
-                messagebox.showinfo(title='填报结果',message='填报失败，请检查网络设置！')    
-                
-    btn = tk.Button(enter_w,text='登陆',fg="black",width=7,compound='center',\
-                      bg = "white",command = lambda :panduan(enter_w))
-    btn.pack()
-    btn.place(x=330,y=270)
-    enter_w.mainloop()    
-
-# 登录按钮事件处理函数
-def yqtb_nwpu(a):
-    #input('请输入账号:')
-    #input('请输入密码:')
-    #print (a[0],a[1])
-    chrome_options = Options()
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--disable-dev-shm-usage')
-    chrome_options.add_argument('--headless')
-    driver = webdriver.Chrome(options=chrome_options)
-    driver.delete_all_cookies()
-    driver.get('http://yqtb.nwpu.edu.cn/wx/xg/yz-mobile/index.jsp')#转到翱翔门户登陆页面
-    time.sleep(6)
-
-    username=driver.find_element(By.ID,'username')
-    username.clear()
-    username.send_keys(a[0])#抓取用户名栏并输入学号
-    password=driver.find_element(By.ID,'password')
-    password.clear()
-    password.send_keys(a[1])#抓取密码栏并输入密码
-    driver.find_element(By.NAME,'submit').click()#抓取登录按钮并点击
-    time.sleep(6)
-
-    driver.get('http://yqtb.nwpu.edu.cn/wx/ry/jrsb.jsp')
-    time.sleep(6)
-    
-    driver.find_element(By.XPATH,'//*[@id="rbxx_div"]/div[3]/label[2]/div[1]/p').click()
-    driver.find_element(By.CLASS_NAME,'weui-btn_primary').click()#抓取提交按钮#提交
-    sub2=driver.find_element(By.ID,'brcn')
-    driver.execute_script("arguments[0].click();", sub2)
-    driver.find_element(By.ID,'save_div').click()
-    driver.quit()
-    messagebox.showinfo(title='填报结果',message='填报成功！')
-
-python_root = os.path.abspath('.')  # .py当前目录
-base_url = 'http://npm.taobao.org/mirrors/chromedriver/'  # chromedriver在国内的镜像网站
-version_re = re.compile(r'^[1-9]\d*\.\d*.\d*')  # 匹配前3位版本信息
-
-def download(link, file_name):
-    response = requests.get(link)
-    file = response.content
-    with open(file_name, 'wb') as f:
-        f.write(file)
-def unzip(zip_file):
-    extracting = zipfile.ZipFile(zip_file)
-    extracting.extractall('.')
-    extracting.close()
-    os.remove(zip_file)
-    
-def get_chrome_version():
-    """通过注册表查询Chrome版本信息: HKEY_CURRENT_USER\SOFTWARE\Google\Chrome\BLBeacon: version"""
-    try:
-        key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, 'SOFTWARE\Google\Chrome\BLBeacon')
-        value = winreg.QueryValueEx(key, 'version')[0]
-        return version_re.findall(value)[0]
-    except WindowsError as e:
-        return '0.0.0'  # 没有安装Chrome浏览器
+import requests
 
 
-def get_chrome_driver_version():
-    try:
-        result = os.popen('chromedriver --version').read()
-        version = result.split(' ')[1]
-        return '.'.join(version.split('.')[:-1])
-    except Exception as e:
-        return '0.0.0'  # 没有安装ChromeDriver
+def logger(text, file="yqtb.log"):
+    with open(file, "a+") as f:
+        f.write(text + '\n')
+        f.close()
 
 
-def get_latest_chrome_driver(chrome_version):
-    url = f'{base_url}LATEST_RELEASE_{chrome_version}'
-    latest_version = requests.get(url).text
-    download_url = f'{base_url}{latest_version}/chromedriver_win32.zip'
-    if os.path.exists('chromedriver.exe'):
-        os.remove('chromedriver.exe')
-    download(download_url, 'chromedriver.zip')
-    unzip('chromedriver.zip')
-    return True
-
-
-def check_chrome_driver_update():
-    messagebox.showinfo(title='提醒',message='请勿打开相关代理软件，否则可能闪退')
-    chrome_version = get_chrome_version()
-    if os.path.isfile("chromedriver.exe"):
-            original_window()
+def yqtb(username, password, name, params):
+    session = requests.session()
+    url = "https://yqtb.nwpu.edu.cn/wx/ry/jrsb.jsp"
+    post_url = "https://yqtb.nwpu.edu.cn/wx/ry/ry_util.jsp"
+    login_url = "https://uis.nwpu.edu.cn/cas/login"
+    login_data = {
+        # 学号
+        'username': username,
+        # 密码
+        'password': password,
+        'currentMenu': '1',
+        'execution': 'e1s1',
+        "_eventId": "submit"
+    }
+    header = {
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 12; M2012K11AC Build/SKQ1.211006.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/104.0.5112.97 Mobile Safari/537.36 uni-app Html5Plus/1.0 (Immersed/29.09091)',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Referer': 'https://yqtb.nwpu.edu.cn/wx/ry/jrsb_xs.jsp',
+    }
+    response = session.get(login_url, headers=header)
+    execution = re.findall(r'name="execution" value="(.*?)"', response.text)[0]
+    login_data['execution'] = execution
+    response = session.post(login_url, data=login_data, headers=header)
+    if "欢迎使用" in response.text:
+        print(name + "login successfully")
     else:
-        print('第一次安装正在更新相关组件')
-        get_latest_chrome_driver(chrome_version)
-        messagebox.showinfo(title='完成',message='更新完成，请重写打开程序')
+        print(name + "login unsuccessfully")
+        exit(1)
+    res = ""
+    for i in range(3):
+        if len(res) == 0:
+            response = session.get("https://yqtb.nwpu.edu.cn/wx/xg/yz-mobile/index.jsp", headers=header)
+            response = session.get("https://yqtb.nwpu.edu.cn/wx/ry/jrsb.jsp", headers=header)
+            pattern = r"url:'ry_util\.jsp\?sign=(.*).*'"
+            res = re.findall(pattern, response.text)
+    logger('res:' + str(res))
+    if len(res) == 0:
+        logger("error in script, please contact to the author")
+        exit(1)
+    time.sleep(5)
+    post_url += "?sign=" + res[0]
+    html = session.get(url)
+    html = session.get(url, headers=header)
+    time.sleep(5)
+    session.headers.update({'referer': 'https://yqtb.nwpu.edu.cn/wx/ry/jrsb.jsp'})
+    time.sleep(5)
+    html = session.post(post_url, data=params, headers=header)
+    result = '{"state":"1"}' in html.text
+    #print(html.status_code)
+    if result:
+        logger(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + name + "信息提交成功")
+    else:
+        logger(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + name + "信息提交失败, HTTP错误代码: " + str(html.status_code))
+
+
+def get_now():
+    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+
+def tianbao(student_info, params):
+    try:
+        params['userLoginId'] = student_info[0]
+        params['userName'] = student_info[2]
+        yqtb(student_info[0], student_info[1], student_info[2], params)
+    except:
+        raise Exception(f'疫情填报运行失败\n{traceback.format_exc()}')
+
+
+student_list = [
+    {
+        "user": ['学号', '密码', '姓名'],#[]内三项必填
+        'params': {
+            "hsjc": "1",
+            "sfczbcqca": "",
+            "czbcqcasjd": "",
+            "sfczbcfhyy": "",
+            "czbcfhyysjd": "",
+            "actionType": "addRbxx",
+            "userLoginId": "$学号",
+            "userName": "$姓名",
+            "szcsbm": "1",   #在学校为1，不在学校不填
+            "bdzt": "1",     #在学校为1，不在学校为0
+            "szcsmc": "在学校", #位置
+            "szcsmc1": "在学校",  #位置
+            "sfjt": "0",
+            "sfjtsm": "",
+            "sfjcry": "0",
+            "sfjcrysm": "",
+            "sfjcqz": "0",
+            "sfyzz": "0",
+            "sfqz": "0",
+            "ycqksm": "",
+            "glqk": "0",
+            "glksrq": "",
+            "gljsrq": "",
+            "tbly": "app",
+            "glyy": "",
+            "qtqksm": "",
+            "sfjcqzsm": "",
+            "sfjkqk": "0",
+            "jkqksm": "",
+            "sfmtbg": "",
+            "sfxn":"2",
+            "sfdw":"1",
+            "longlat":"108.914253,34.242023", #经纬度
+            "userType": "2",
+            "qrlxzt": "3",
+            "xymc": "",
+            "xssjhm": ""
+        }
+    }
+]
+ 
+def send_rtx(msg):
+    webhook = r"https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=552647ec-b0fc-42c3-961c-cae0bed49fcd"
+    data = {
+        'msgtype': 'text',
+        'text': {
+            'content': msg,
+            "mentioned_mobile_list": ["@郑博文"]
+        }
+    }
+    res = requests.post(webhook, data=json.dumps(data))
+    if res.status_code == 200:
+        return True
+    else:
+        send_rtx(msg)
 
 
 if __name__ == '__main__':
-    check_chrome_driver_update()
+    # 前一个参数为列表，参考上面的list示例
+    # 后一个参数为想填报的信息，在学校选择params_school，在家选择prams_home，具体参数参见注释，不定期更新
+    import time
+    import datetime
+    while True:
+        logger(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + "疫情填报脚本开始运行")
+        for student in student_list:
+            try:
+                logger(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + student.get('user')[2] + '开始填报')
+                tianbao(student.get('user'), student.get('params'))
+                time.sleep(30)
+            except Exception as e:
+                send_rtx(str(e))
+        logger(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + "疫情填报脚本运行结束")
+        time.sleep(3600 * 6)
